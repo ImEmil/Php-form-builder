efc24ef3e8e22b4e09de10b1a32642688b7b1e0e
Emil Nilsson <emil_nilsson_96@live.se>
2014-01-18 01:37:07 +0100

    Added a validation function

--- class/class.form_builder.php
+++ class/class.form_builder.php
@@ -1,51 +1,52 @@
 <?php
-/*
-                                                   +-+-+-+-+-+-+
-                                                   |I|m|E|m|i|l|
-                                                   +-+-+-+-+-+-+
-                                         
-*/
 class Form {
-	public $action, $inputs, $former, $data, $name, $input = [], $method = "GET";
+	public $action, $inputs, $former, $data, $name, $input = [], $method = "POST";
+
+	static $required = true;	// Default true, set it to false if you don't want any requirement
 
 	function __construct($action, $name) {
 		$this->action = $action;
 		$this->name   = $name;
 	}
 
+	static function filter($string) {
+		return trim(strip_tags(htmlspecialchars( $string, ENT_QUOTES, 'utf-8')));
+	}
+
+	static function redirect($page, $seconds) {
+		echo("<meta http-equiv=\"refresh\" content=\"{$seconds};url={$page}\" />");
+	}
+
 	public function inputs($input) {
 		$this->inputs[$input];
 		foreach($input as $this->inputs => $input) {
-			$this->data   .=  $this->inputs;
-			$this->former .=
-			sprintf("
-			<label for='%s'> %s: </label>
-			<input type='text' name='%s' id='%s' placeholder='Type in %s'> <br>",
+			$this->throwError($this->inputs, "Type in your {$this->inputs}");
+			$this->data   .=  self::filter(isset($_POST[$this->inputs]));	// Every input has it's value  stored in $this->data variable
+			$this->former .=  sprintf("<label for='%s'> %s: </label> <input type='text' name='%s' id='%s' placeholder='Type in %s'> <br>",
 			$this->inputs ,$input, $this->inputs, $this->inputs, $input);
+		}
+	}
+
+	public function throwError($input, $msg) {
+		if(Form::$required == true && isset($_POST[$input])) {
+		 $input = self::filter($_POST[$input]);
+		 return(empty($input) ? self::redirect($_SERVER['HTTP_REFERER'], 3) . die("Error: {$msg}") : false);
 		}
 	}
 
 	public function button($name, $value) {
 		return "<button type=\"submit\" name=\"{$name}\">{$value}</button>";
 	}
-
+	
 	public function render($value) {
-		if(!$this->control()) {
+		if(!$this->submitted()) {
 		echo
 		sprintf("<form method=\"%s\" action=\"%s\">%s %s</form>",
 			$this->method, $this->action, $this->former, $this->button($this->name, $value));
 			}
-			else {
-				echo "Successful!";
-			}
 	}
-
-	public function filter($string) {
-		return trim(strip_tags(htmlspecialchars( $string, ENT_QUOTES, 'utf-8')));
-	}
-
-	public function control() {
-	  //$this->filter($this->data)
-		return(isset($_GET[$this->name]) ? true : false);
+	
+	public function submitted() {
+		return(isset($_POST[$this->name]) ? true : false);
 	}
 }
